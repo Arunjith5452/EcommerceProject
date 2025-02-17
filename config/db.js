1e20f16361d344ba45b7019f8b9fb8e7ddc6b500
Arunjith5452 <arunjith.5452@gmail.com>
2025-02-17 10:25:22 +0530

    fix(product): resolve issue with adding product image

--- config/db.js
+++ config/db.js
@@ -6,7 +6,11 @@ const env = require('dotenv').config()
 const connectDB = async () => {
   try {
 
-    await mongoose.connect(process.env.MONGODB_URI)
+    const conn = await mongoose.connect(process.env.MONGODB_URI)
+    
+    console.log("DB host name",conn.connection.host)
+    console.log("DB name oke make sure the db name",conn.connection.name)
+
     console.log("DB connected")
 
   } catch (error) {
